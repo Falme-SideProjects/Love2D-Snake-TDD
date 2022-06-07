59b8599f251ceca9fe3e8d45e75cde1995b3bf70
Falme Streamless <falme_streamless@hotmail.com>
2022-06-06 22:01:34 -0300

    Made 500x500 window configuration

---
+++ Game/conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.window.width = 500
+    t.window.height = 500
+	t.modules.joystick = false
+    t.modules.physics = false
+	t.window.title = "Snake"
+	t.window.resizable = false
+end
